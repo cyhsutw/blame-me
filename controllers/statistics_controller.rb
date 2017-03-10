112d388bb68519719a7de99faa1055a9e021f541
Cheng-Yu Hsu <m@cyhsu.me>
2017-03-10 14:49:44 +0800

    Dummy implementation of statistics controller

---
+++ controllers/statistics_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class BlameMe < Sinatra::Base
+  get '/statistics' do
+    slim :statistics
+  end
+
+  post '/statistics' do
+
+    redirect to('/statistics')
+  end
+end
