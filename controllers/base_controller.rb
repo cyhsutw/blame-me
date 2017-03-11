9b87b1a9eae7e4babcabe0bf1205685563c1e4ab
Cheng-Yu Hsu <m@cyhsu.me>
2017-03-11 16:07:42 +0800

    Remove session related code

--- controllers/base_controller.rb
+++ controllers/base_controller.rb
@@ -3,10 +3,6 @@
 class BlameMe < Sinatra::Base
   register Sinatra::AssetPack
 
-  use Rack::Session::Cookie, key: 'rack.session',
-                             expire_after: 3600,
-                             secret: ENV['RACK_SESSION_SECRET']
-
   set :views, File.expand_path('../views', __dir__)
   set :public_folder, File.expand_path('../assets', __dir__)
 
