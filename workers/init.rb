095e66a9d5f38dd7717224abc449393916939f15
Cheng-Yu Hsu <m@cyhsu.me>
2017-03-11 15:00:00 +0800

    Config redis

--- workers/init.rb
+++ workers/init.rb
@@ -2,7 +2,9 @@
 require 'sidekiq'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{ENV['REDIS_HOST'] || 'localhost'}:#{ENV['REDIS_PORT'] || 6379}" }
+  config.redis = {
+    url: ENV['REDIS_URL'] || "redis://#{ENV['REDIS_HOST'] || 'localhost'}:#{ENV['REDIS_PORT'] || 6379}"
+  }
 end
 
 Dir.glob("#{File.dirname(__FILE__)}/*.rb").each do |file|
